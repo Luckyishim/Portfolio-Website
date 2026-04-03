4ebe60d22efc3509687a92573391702a844634ee
unknown <luckyrajkarnikar@gmail.com git config --global user.name Luckyishim>
2026-04-03 23:56:42 +0545

    Now it should work

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -24,7 +24,7 @@ export const AboutSection = () => {
                             Get In Touch
                         </a>
                         {/* CV ko link halne yesma pachi */}
-                        <a href=""
+                        <a href="projects/C.V_Lucky_Rajkarnikar.pdf"
                             className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300" >
                             Download CV
                         </a>
